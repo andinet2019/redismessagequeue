1cc82218f5734b004a40916c8f9814fc57e90b94
AndinetGebre <Andinet.Gebre@target.com>
2021-08-09 22:03:54 -0500

    message

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+import redis
+from rq import Queue
+import time
+app=Flask(__name__)
+r=redis.Redis()
+q=Queue(connection=r)
+
+def background_task(n):
+
+    """ Function that returns len(n) and simulates a delay """
+
+    delay = 2
+
+    print("Task running")
+    print(f"Simulating a {delay} second delay")
+
+    time.sleep(delay)
+
+    print(len(n))
+    print("Task complete")
+
+    return len(n)
+
+@app.route("/task")
+def index():
+
+    if request.args.get("n"):
+
+        job = q.enqueue(background_task, request.args.get("n"))
+        q_len=len(q)
+        return f"Task ({job.id}) added to queue at {job.enqueued_at} {q_len} tasks in the queue"
+
+    return "No value for count provided"
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+#make sure to start rq worker in same directory
+
+if __name__=='__main__':
+    app.run(debug=True)
